b799556feef3b6f83e544c4009a411a32025d016
AnithaVasu83 <anithavasu83@gmail.com>
2026-01-08 23:08:33 +0530

    fourth commit

--- src/task1_string_manipulation/ConcatForLoop.java
+++ src/task1_string_manipulation/ConcatForLoop.java
@@ -11,6 +11,7 @@ public class ConcatForLoop {
         for (int i = 0; i < s1.length(); i++) {
             result[k++] = s1.charAt(i);
         }
+
         for (int i = 0; i < s2.length(); i++) {
             result[k++] = s2.charAt(i);
         }
